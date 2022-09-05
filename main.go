02c61ce63c7507638006e0c1721eb41dddc7b0a8
Pavel Sorokin <60606414+pavel-snyk@users.noreply.github.com>
2022-09-05 02:20:56 +0200

    chore: set provider version via ldflags when releasing

--- main.go
+++ main.go
@@ -10,13 +10,17 @@ import (
 	"github.com/pavel-snyk/terraform-provider-snyk/internal/provider"
 )
 
+var (
+	version = "dev"
+)
+
 func main() {
 	var debugMode bool
 
 	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with debug support")
 	flag.Parse()
 
-	err := providerserver.Serve(context.Background(), provider.New, providerserver.ServeOpts{
+	err := providerserver.Serve(context.Background(), provider.New(version), providerserver.ServeOpts{
 		Address: "registry.terraform.io/pavel-snyk/snyk",
 		Debug:   debugMode,
 	})
