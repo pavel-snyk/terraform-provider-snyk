68b1601254765697edd40d33ee552e2ffb4e19ff
Pavel Sorokin <60606414+pavel-snyk@users.noreply.github.com>
2026-02-17 02:12:25 +0100

    docs(datasource/snyk_app_install): update documentation with examples

---
+++ examples/data-sources/snyk_app_install/data-source.tf
@@ -0,0 +1,16 @@
+data "snyk_app_install" "unicorn" {
+  organization_id = data.snyk_organization.frontend.id
+
+  # Search criteria to find the app installation. At least one of
+  # the following must be provided. Combining multiple attributes
+  # makes the search more specific. For example, providing both
+  # `id` and `app_name` will ensure that the app installation with
+  # the given `id` also has the correct name, preventing potential mismatches.
+  id       = "<app-install-id>"
+  app_id   = "<app-id>"
+  app_name = "<app-name>"
+}
+
+data "snyk_organization" "frontend" {
+  name = "Frontend Team"
+}
